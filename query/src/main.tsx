38e93f9376542e941e2e8c75f378835fc79634e0
Aman Kumar <amankarguwal0@gmail.com>
2024-07-07 17:58:41 +0530

    chore: Update main.tsx to use Redux store and Provider component

--- query/src/main.tsx
+++ query/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+// import { ApiProvider } from "@reduxjs/toolkit/query/react";
+// import { myApi } from "./redux/api.ts";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      {/* <ApiProvider api={postsApi}> */}
+        <App />
+      {/* </ApiProvider> */}
+    </Provider>
+  </React.StrictMode>
+);
