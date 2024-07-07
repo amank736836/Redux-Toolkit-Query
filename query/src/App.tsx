683d26860802f155f8054b767d89805b4c4fd095
Aman Kumar <amankarguwal0@gmail.com>
2024-07-07 17:58:26 +0530

    chore: Refactor App component and add new post functionality

--- query/src/App.tsx
+++ query/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import PostsCard from "./components/PostsCard";
+import { useGetPostsQuery, useNewPostMutation } from "./redux/api";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const { isLoading, isError, isSuccess, data, error } = useGetPostsQuery("");
+  const { isLoading, data } = useGetPostsQuery("");
+  console.log(isLoading, data);
+  const [newPost] = useNewPostMutation();
+
+  const [title, setTitle] = useState<string>("");
+  const [body, setBody] = useState<string>("");
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const post:Post = {
+      title,
+      body,
+      userId: Math.floor(Math.random() * 1000),
+      id: Math.floor(Math.random() * 1000),
+    }
+    newPost(post);
+    setTitle("");
+    setBody("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>MyApp</h1>
+
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Body"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+
+        <button type="submit">Add</button>
+      </form>
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        data?.map((post) => <PostsCard key={post.id} post={post} />)
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
